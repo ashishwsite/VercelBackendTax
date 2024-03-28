33c89dafcb7f0bd6deb217a5851a48180f11580c
ashish <1508ramashish@gmail.com>
2024-03-28 11:26:01 +0530

    cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const express= require('express')
-var cors=require('cors')
+const  cors=require('cors')
 require('./db')
 // const port=process.env.PORT||3000/
 const port =3000
 const path = require("path");
 const app=express()
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use('/',require('./Routes/Auth'))
 app.get("/",(req,res)=>{
   res.json("default")
